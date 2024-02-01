6eaf14126f6d6af618286595104a2c06d75e4dc7
thaisarten <tarten@gmail.com>
2024-02-01 11:41:15 -0300

    Limpando a string via Regex
    Resolve o mesmo problema que antes era abordado via for loop. Agora utilizando o regex

--- script.js
+++ script.js
@@ -6,11 +6,6 @@ const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
 let cleanInputStrig = function (str) {
-    const strArray = str.split(''); //Divide uma string em substrings q são colocadas em uma array
-    const cleanStrArray = [];
-    for (let i = 0; i < strArray.length; i++) {
-        if(!['+','-',' '].includes(strArray[i])){
-            cleanStrArray.push(strArray[i]); //
-        }
-    } //vai iterar cada caracter q foi armazenado na strArray. Cada um desses elementos será armazenado na array cleanStrArray.
+    const regex = /[+-\s]/g;
+    return str.replace(regex, '');      
 }
