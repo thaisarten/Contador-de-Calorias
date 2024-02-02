a21410341322f1c67763c9b8030e16ce8f909da8
thaisarten <tarten@gmail.com>
2024-02-02 00:57:51 -0300

    Criando a função getCaloriesFromInputs( )

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
+
 let isError = false;
-let cleanInputStrig = function (str) {//passos 18 a 22 (for loop) e 23 a 28 (regex)
+
+let cleanInputString = function (str) {//passos 18 a 22 (for loop) e 23 a 28 (regex)
     const regex = /[+-\s]/g;
     return str.replace(regex, '');
 }
+
 function isInvalidInput(str) { //passos 29 a 35
     const regex = /\d+e\d+/i; // aqui não precisamos assinalar que é global (g) pq há apenas um valor. No entando, utilizamos a flag i para que o padrão regex se torne "case-insensitive"
     return str.match(regex);
 }
+
 function addEntry() { //passos 36 a 49 e 51 a 53
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
@@ -25,3 +29,17 @@ function addEntry() { //passos 36 a 49 e 51 a 53
     targetInputContainer.insertAdjacentHTML('beforeend', HTMLString)
 }
 addEntryButton.addEventListener('click', addEntry);//passo 50
+function getCaloriesFromInputs(list) { // passos 54 a 64
+    let calories = 0;
+    for (let i = 0; i < list.length; i++) {
+        const currVal = cleanInputString(list[i].value);
+        const invalidInputMatch = isInvalidInput(currVal);
+        if (invalidInputMatch) {
+            alert(`Invalid Input: ${invalidInputMatch[0]}`);
+            isError = true;
+            return null;
+        }
+        calories += Number(currVal);
+    }
+    return calories;
+}
